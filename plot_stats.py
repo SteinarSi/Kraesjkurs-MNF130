46c2b0da1bd6383be91635a1b6493569d90aed66
Steinar <steinar.simonnes@gmail.com>
2024-05-22 11:46:40 +0200

    Last minute changes

---
+++ plot_stats.py
@@ -0,0 +1,14 @@
+
+import matplotlib.pyplot as plt
+
+result = 300
+
+years = [2022, 2023, 2024]
+results = [84, 64, result]
+
+plt.bar(years, results)
+plt.xticks(years)
+plt.xlabel("År")
+plt.ylabel("Sekunder")
+plt.title("Antall sekunder før noen turte å si ifra om at det var feil kræsjkurs")
+plt.show()
